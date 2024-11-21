0c2ef727797a71795e4f8574d67b115e625cec66
KvasirSG <12102444+KvasirSG@users.noreply.github.com>
2024-11-21 11:28:49 +0000

    Initial commit

---
+++ src/main/java/f24c2c1/projektkalkulering/ProjektKalkuleringApplication.java
@@ -0,0 +1,13 @@
+package f24c2c1.projektkalkulering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektKalkuleringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektKalkuleringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/f24c2c1/projektkalkulering/ProjektKalkuleringApplicationTests.java
@@ -0,0 +1,13 @@
+package f24c2c1.projektkalkulering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektKalkuleringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
