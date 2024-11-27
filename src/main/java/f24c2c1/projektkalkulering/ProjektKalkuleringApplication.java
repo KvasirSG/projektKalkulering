2582cc5e7a52ea8fd2b637ea0ec3d334a142fe39
KvasirSG <12102444+KvasirSG@users.noreply.github.com>
2024-11-27 10:04:46 +0000

    adding UserService with password hashing for password security.

--- src/main/java/f24c2c1/projektkalkulering/ProjektKalkuleringApplication.java
+++ src/main/java/f24c2c1/projektkalkulering/ProjektKalkuleringApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjektKalkuleringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjektKalkuleringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektKalkuleringApplication.class, args);
+    }
 
 }
