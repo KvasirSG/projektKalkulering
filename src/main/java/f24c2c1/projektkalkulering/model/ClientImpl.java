79a1a8eb7f03fdbe5a8001b2f11d7542af40d38d
KvasirSG <12102444+KvasirSG@users.noreply.github.com>
2024-12-06 10:19:51 +0000

    added client implementation

---
+++ src/main/java/f24c2c1/projektkalkulering/model/ClientImpl.java
@@ -0,0 +1,92 @@
+package f24c2c1.projektkalkulering.model;
+
+public class ClientImpl implements Client {
+    private long id;
+    private String name;
+    private String contactName;
+    private String address;
+    private String city;
+    private String zip;
+    private String phone;
+    private String email;
+
+    @Override
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getContactName() {
+        return contactName;
+    }
+
+    @Override
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    @Override
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String getCity() {
+        return city;
+    }
+
+    @Override
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String getZip() {
+        return zip;
+    }
+
+    @Override
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    @Override
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
